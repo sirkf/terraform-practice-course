54109041913a66858aeb291d6bb4e70ecfc92541
sirkf <sir.k.floyd@gmail.com>
2025-11-23 19:59:11 -0500

    add shared_credentials_files

--- main.tf
+++ main.tf
@@ -1,3 +1,5 @@
-resource "random_string" "random" {
-  length = 10
+# Configure the AWS Provider
+provider "aws" {
+  region                   = "us-east-1"
+  shared_credentials_files = "$HOME/.aws/credentials"
 }
