8c5d4d8728e760a90e7471d7cb9826a65391319c
sirkf <sir.k.floyd@gmail.com>
2025-11-30 14:49:30 -0500

    add two more outputs

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,13 @@ output "vpc_id" {
   description = "Output the ID for the primary VPC"
   value       = aws_vpc.vpc.id
 }
+
+output "public_url" {
+  description = "Public URL for our Web Server"
+  value       = "https://${aws_instance.web_server.public_ip}:8080/index.html"
+}
+
+output "vpc_information" {
+  description = "VPC Information about Environment"
+  value       = "Your ${aws_vpc.vpc.tags.Environment} VPC has an ID of ${aws_vpc.vpc.id}"
+}
