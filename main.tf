b205c1ac05401325b1d6b14c10ed626b700945f0
sirkf <sir.k.floyd@gmail.com>
2025-12-04 18:49:16 -0500

    add server folder where module lives in, add another vm from module.server

--- main.tf
+++ main.tf
@@ -361,3 +361,20 @@ module "server" {
     aws_security_group.vpc-web.id
   ]
 }
+module "server_subnet_1" {
+  source    = "./server"
+  ami       = data.aws_ami.ubuntu.id
+  subnet_id = aws_subnet.public_subnets["public_subnet_1"].id
+  security_groups = [
+    aws_security_group.vpc-ping.id,
+    aws_security_group.ingress-ssh.id,
+    aws_security_group.vpc-web.id
+  ]
+}
+output "public_ip" {
+  value = module.server.public_ip
+}
+
+output "public_dns" {
+  value = module.server.public_dns
+}
