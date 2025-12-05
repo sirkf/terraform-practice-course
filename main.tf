5537810795bec183208317c4c09d217527cba9f7
sirkf <sir.k.floyd@gmail.com>
2025-12-04 20:38:32 -0500

    add autoscaling to main.tf and source from terraform registry/github

--- main.tf
+++ main.tf
@@ -27,7 +27,7 @@ resource "aws_vpc" "vpc" {
     Name        = var.vpc_name
     Environment = "demo_environment"
     Terraform   = "true"
-    Region      = data.aws_region.current.name
+    Region      = data.aws_region.current.id
   }
 }
 
@@ -362,14 +362,15 @@ module "server" {
   ]
 }
 module "server_subnet_1" {
-  source    = "./modules/server"
-  ami       = data.aws_ami.ubuntu.id
-  subnet_id = aws_subnet.public_subnets["public_subnet_1"].id
-  security_groups = [
-    aws_security_group.vpc-ping.id,
+  source      = "./modules/web_server"
+  ami         = data.aws_ami.ubuntu.id
+  key_name    = aws_key_pair.generated.key_name
+  user        = "ubuntu"
+  private_key = tls_private_key.generated.private_key_pem
+  subnet_id   = aws_subnet.public_subnets["public_subnet_1"].id
+  security_groups = [aws_security_group.vpc-ping.id,
     aws_security_group.ingress-ssh.id,
-    aws_security_group.vpc-web.id
-  ]
+  aws_security_group.vpc-web.id]
 }
 output "public_ip" {
   value = module.server.public_ip
@@ -384,4 +385,28 @@ output "public_ip_server_subnet_1" {
 
 output "public_dns_server_subnet_1" {
   value = module.server_subnet_1.public_dns
+}
+
+module "autoscaling" {
+  source = "github.com/terraform-aws-modules/terraform-aws-autoscaling"
+
+  # Autoscaling group
+  name = "myasg"
+
+  vpc_zone_identifier = [aws_subnet.private_subnets["private_subnet_1"].id,
+    aws_subnet.private_subnets["private_subnet_2"].id,
+  aws_subnet.private_subnets["private_subnet_3"].id]
+  min_size         = 0
+  max_size         = 1
+  desired_capacity = 1
+
+  # Launch template
+  image_id      = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+  instance_name = "asg-instance"
+
+  tags = {
+    Name = "Web EC2 Server 2"
+  }
+
 }
