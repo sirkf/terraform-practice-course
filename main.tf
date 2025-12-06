a83ad465ed83db6e506628ce0db123b55985a1b5
sirkf <sir.k.floyd@gmail.com>
2025-12-06 09:46:54 -0500

    add '2' to  Web EC2 Server

--- main.tf
+++ main.tf
@@ -355,6 +355,6 @@ resource "aws_instance" "web_server_2" {
   instance_type = "t3.micro"
   subnet_id     = aws_subnet.public_subnets["public_subnet_2"].id
   tags = {
-    Name = "Web EC2 Server"
+    Name = "Web EC2 Server 2"
   }
 }
